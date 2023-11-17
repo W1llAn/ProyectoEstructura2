68898d2de087e84d47f165c849be6255c4e2730c
W1llAn <wchimborazo2840@uta.edu.ec>
2023-11-17 09:27:56 -0500

    Registros William

---
+++ ListaAeropuerto/src/listaaeropuerto/Reportes.java
@@ -0,0 +1,46 @@
+package listaaeropuerto;
+
+import java.util.ArrayList;
+
+public class Reportes {
+    public void listaPasajerosVuelo(ArrayList<Pasajero> pasajeros,ArrayList<Vuelo> vuelos){
+        System.out.println("Vuelos:");
+        for (Vuelo vuelo : vuelos) {
+            System.out.println("Destino"+vuelo.getDestino());
+            for (Pasajero pasajero : pasajeros) {
+                if (pasajero.getDestinoViaja().equals(vuelo.getDestino())) {
+                    System.out.println("-"+pasajero.getNombres()+" "+pasajero.getApellidos());
+                }   
+            }
+        }
+
+    }
+    public void cantPersonasDestinos(ArrayList<Pasajero> pasajeros,ArrayList<Vuelo> vuelos){
+        System.out.println("Vuelos:");
+        for (Vuelo vuelo : vuelos) {
+            int cantPasajeros=0;
+            for (Pasajero pasajero : pasajeros) {
+                if (pasajero.getDestinoViaja().equals(vuelo.getDestino())) {
+                 cantPasajeros++;   
+                }
+                System.out.println("Destino "+vuelo.getDestino()+" : "+cantPasajeros);  
+            }
+
+        }
+    }
+    public void cantPersonasDestinosListaEspera(ArrayList<Vuelo> vuelos,ArrayList<Pasajero> pasajeros){
+        System.out.println("Lista actual de espera:");
+        for (Vuelo vuelo : vuelos) {
+            int cantPasajeros=0;
+            for (Pasajero pasajero : pasajeros) {
+                if (pasajero.getDestinoViaja().equals(vuelo.getDestino())) {
+                 cantPasajeros++;   
+                }   
+                double porcentaje=0;
+                porcentaje = (cantPasajeros*100)/pasajeros.size();
+                System.out.println("Destino "+vuelo.getDestino()+" : "+porcentaje+"%");  
+            }
+
+        }
+    }
+}
